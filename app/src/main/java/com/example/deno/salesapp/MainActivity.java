89ea3a5b9ca5ac9039b799ddca371c745423aa1f
Denis Muthomi <muthomidenis62@gmail.com>
2018-11-12 08:33:39 +0300

    second commit

--- app/src/main/java/com/example/deno/salesapp/MainActivity.java
+++ app/src/main/java/com/example/deno/salesapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.deno.salesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+
+import example.abhiandroid.MyApplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //initiate button
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+
+                //initiate button
+                final Button button = findViewById(R.id.button2);
+                button.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        // Code here executes on main thread after user presses button
+                    }
+                });
+            }
+        });
     }
 }
+
+
+
